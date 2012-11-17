66bc73f55cea66e5c9c8b9dd1fc6099fac41812e
endroid <info@endroid.nl>
2012-11-17 11:30:49 -0500

    Enable urls as text

--- Controller/QrCodeController.php
+++ Controller/QrCodeController.php
@@ -16,7 +16,7 @@ class QrCodeController extends Controller
 {
     /**
      *
-     * @Route("/{text}.{extension}", name="endroid_qrcode", requirements={"extension"="jpg|png|gif"})
+     * @Route("/{text}.{extension}", name="endroid_qrcode", requirements={"text"="[^/\\]+", "extension"="jpg|png|gif"})
      *
      */
     public function generateAction($text, $extension)
