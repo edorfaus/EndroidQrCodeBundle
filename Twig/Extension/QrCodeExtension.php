7b96f50f11c5ff0a62e486cbdc8ce91e36d4b2e6
endroid <info@endroid.nl>
2013-03-31 15:32:45 -0400

    Update licence info

--- Twig/Extension/QrCodeExtension.php
+++ Twig/Extension/QrCodeExtension.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * (c) Jeroen van den Enden <info@endroid.nl>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Endroid\Bundle\QrCodeBundle\Twig\Extension;
 
 use Twig_Extension;
